61bb2678a2c2993eefa3abebbf94fdbef15cb828
ossi <asman00000@mail.ru>
2023-06-11 15:39:21 +0300

    игнор

--- Csharp/лекция2/Program.cs
+++ Csharp/лекция2/Program.cs
@@ -1,28 +1,28 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿// int Max(int arg1, int arg2, int arg3)
 
-{
+// {
 
-    int Max = arg1
-    if(arg2> max) Max = arg2
-    if(arg3> max) Max = arg3
-    return Max
+//     int Max = arg1
+//     if(arg2> max) Max = arg2
+//     if(arg3> max) Max = arg3
+//     return Max
 
-}
+// }
 
-int a1 = 1
-int b1 = 11
-int c1 = 12
+// int a1 = 1
+// int b1 = 11
+// int c1 = 12
 
-int a2 = 13
-int b2 = 14
-int c2 = 15
+// int a2 = 13
+// int b2 = 14
+// int c2 = 15
 
-int a3 = 16
-int b3 = 17
-int c3 = 18
+// int a3 = 16
+// int b3 = 17
+// int c3 = 18
 
-int max1 = max (a1, b1, c1);
-int max2 = max (a2, b2, c2);
-int max3 = max (a3, b3, c3);
-int Max = max(max1, max2, max3);
-System.Console.WriteLine(res)
+// int max1 = max (a1, b1, c1);
+// int max2 = max (a2, b2, c2);
+// int max3 = max (a3, b3, c3);
+// int Max = max(max1, max2, max3);
+// System.Console.WriteLine(res)
