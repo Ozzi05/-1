10235ce4cec86e2da102244b973a091874ad63fb
ossi <asman00000@mail.ru>
2023-06-11 10:59:31 +0300

    задача6семин1

---
+++ Csharp/задача6семин1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число :");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA % 2 ==0)
+{
+    System.Console.WriteLine("четное");
+}
+else
+{
+    System.Console.WriteLine("нечетное");
+}
