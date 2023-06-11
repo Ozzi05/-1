8e99b47e2513e086d0105765a23341dff6a5427e
ossi <asman00000@mail.ru>
2023-06-11 12:37:31 +0300

    лекция2

---
+++ Csharp/лекция2/Program.cs
@@ -0,0 +1,25 @@
+﻿int max(int arg1, int arg2, int arg3)
+{
+    int res
+    if(arg2> res) res = arg2
+    if(arg3> res) res = arg3
+    return res
+}
+
+int a1 = 1
+int b1 = 11
+int c1 = 12
+
+int a2 = 13
+int b2 = 14
+int c2 = 15
+
+int a3 = 16
+int b3 = 17
+int c3 = 18
+
+int max = max (a1, b1, c1)
+int max = max (a2, b2, c2)
+int max = max (a3, b3, c3)
+int max = max(max1, max2, max3)
+System.Console.WriteLine(max1);
